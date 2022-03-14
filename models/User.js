babd5f4455fb4d768507498d9e898fdd00f71968
Kayce Heap <kayce.heap@gmail.com>
2022-03-14 10:05:53 -0600

    Working on models for users and thoughts

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+// create the Pizza model using the PizzaSchema
+const User = model('User', UserSchema);
+
+// export the Pizza model
+module.exports = User;
